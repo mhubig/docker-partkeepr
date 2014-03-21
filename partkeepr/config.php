96cec99dbcdbc0a3e1d7be3576a2246f0b7e5971
Markus Hubig <mhubig@gmail.com>
2014-03-21 12:44:54 +0100

    Removes the complicated password.
    Signed-off-by: Markus Hubig <mhubig@gmail.com>

--- partkeepr/config.php
+++ partkeepr/config.php
@@ -4,6 +4,6 @@ use PartKeepr\Util\Configuration;
 
 Configuration::setOption("partkeepr.database.dbname", "partkeepr");
 Configuration::setOption("partkeepr.database.username", "partkeepr");
-Configuration::setOption("partkeepr.database.password", "Ilfyafbud6");
+Configuration::setOption("partkeepr.database.password", "partkeepr");
 Configuration::setOption("partkeepr.database.host", "127.0.0.1");
 Configuration::setOption("partkeepr.database.driver", "pdo_mysql");
